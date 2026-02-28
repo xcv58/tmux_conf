719f7b98df4d5d751d983bff3250ccde701e9b99
xcv58 <i@xcv58.com>
2026-02-28 17:02:32 -0500

    Refine tmux time display styling and restore PREFIX+t clock mode

--- tmux-pst.sh
+++ tmux-pst.sh
@@ -1,20 +1,26 @@
 #!/bin/sh
 pd=$(TZ=America/Los_Angeles date +%Y%m%d)
 pd_fmt=$(TZ=America/Los_Angeles date +%m/%d)
+pd_day_raw=$(TZ=America/Los_Angeles date +%a)
 pt=$(TZ=America/Los_Angeles date +%H:%M)
 ud=$(date -u +%Y%m%d)
 ut=$(date -u +%H:%M)
 ld=$(date +%Y%m%d)
 lt=$(date +%H:%M)
 
+case "$pd_day_raw" in
+  Mon) pd_day="Mo" ;;
+  Tue) pd_day="Tu" ;;
+  Wed) pd_day="We" ;;
+  Thu) pd_day="Th" ;;
+  Fri) pd_day="Fr" ;;
+  Sat) pd_day="Sa" ;;
+  Sun) pd_day="Su" ;;
+  *) pd_day="$pd_day_raw" ;;
+esac
+
 # Day offsets relative to PST date
 if [ "$ud" -eq "$pd" ]; then u_s=""; elif [ "$ud" -gt "$pd" ]; then u_s="+1"; else u_s="-1"; fi
 if [ "$ld" -eq "$pd" ]; then l_s=""; elif [ "$ld" -gt "$pd" ]; then l_s="+1"; else l_s="-1"; fi
 
-if [ "$lt" = "$pt" ] && [ "$ld" = "$pd" ]; then
-  echo "$pd_fmt P $pt Z ${ut}${u_s}"
-elif [ "$lt" = "$ut" ] && [ "$ld" = "$ud" ]; then
-  echo "$pd_fmt P $pt Z ${ut}${u_s}"
-else
-  echo "$pd_fmt L ${lt}${l_s} P $pt Z ${ut}${u_s}"
-fi
+echo "#[fg=white,dim]$pd_day $pd_fmt #[fg=cyan,dim]L${lt}${l_s} #[fg=yellow]P${pt} #[fg=magenta,dim]Z${ut}${u_s}#[default]"
